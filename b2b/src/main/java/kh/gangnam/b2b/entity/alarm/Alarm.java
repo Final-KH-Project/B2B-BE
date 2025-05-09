23ad30fc6c2a0cfff2a5ad920cfe39c448dd1dff
Nuri0628 <nuri030628@gmail.com>
2025-05-09 20:20:51 +0900

    [feat] alarm 기능 Dto ãSaveAlarm,ReadAlarm,AAlarm Entity 개발

--- b2b/src/main/java/kh/gangnam/b2b/entity/alarm/Alarm.java
+++ b2b/src/main/java/kh/gangnam/b2b/entity/alarm/Alarm.java
@@ -1,6 +1,53 @@
 package kh.gangnam.b2b.entity.alarm;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import kh.gangnam.b2b.entity.CreatedAt;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Alarm {
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor // 기본 생성자 생성
+@Entity
+public class Alarm extends CreatedAt {
+
+    @Id // 기본 키(PK)
+    @GeneratedValue (strategy = GenerationType.AUTO) // 기본키 값 자동 생성
+    //각 알림에 대한 아이디 부여, DB에 저장할 때 식별자로 사용
+    private Long alarmId;
+
+    //알림 받을 사용자
+    @Column(nullable = false)
+    private Long userId;
+
+//    @Column(nullable = false)
+//    private String password;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String pageUrl;
+
+    //알림 내용
+    @Column(nullable = false)
+    private String content;
+
+    //읽음 여부 처리
+    @Column(nullable = false)
+    private boolean isRead=false;
+
+
+    public  Alarm(Long userId, String content){
+        this.userId=userId;
+        this.content=content;
+    }
+
+    public void markAsRead(){
+        this.isRead=true;
+    }
+
 }
