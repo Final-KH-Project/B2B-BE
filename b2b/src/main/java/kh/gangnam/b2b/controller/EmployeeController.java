e8fefce51911e17180f2a5a7ead4b4f8ed8d2eea
euihyunee <lobil7782@gmail.com>
2025-05-30 11:07:38 +0900

    [feat] Global 예외처리 ResponseStatusException 메시지 형식 수정

--- b2b/src/main/java/kh/gangnam/b2b/controller/EmployeeController.java
+++ b2b/src/main/java/kh/gangnam/b2b/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/mypage")
 public class EmployeeController {
 
     private final EmployeeServiceImpl employeeService;
