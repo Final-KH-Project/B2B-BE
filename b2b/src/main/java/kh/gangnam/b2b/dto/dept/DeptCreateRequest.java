d40da08513fc8eb3b4d8e17adb1a586bae6293a4
euihyunee <lobil7782@gmail.com>
2025-06-04 17:15:20 +0900

    [fix] Dept 비즈니스 로직 수정

--- b2b/src/main/java/kh/gangnam/b2b/dto/dept/DeptCreateRequest.java
+++ b2b/src/main/java/kh/gangnam/b2b/dto/dept/DeptCreateRequest.java
@@ -1,5 +1,7 @@
 package kh.gangnam.b2b.dto.dept;
 
+import kh.gangnam.b2b.entity.Dept;
+import kh.gangnam.b2b.entity.auth.Employee;
 import lombok.*;
 
 @Getter
@@ -14,4 +16,13 @@ public class DeptCreateRequest {
     private Long parentDeptId;
     // 부서장 EmployeeId (nullables)
     private Long headId;
+
+    public Dept toEntity(Employee head, Dept parentDept) {
+        return Dept.builder()
+                .deptName(this.deptName)
+                .location(this.location)
+                .parentDept(parentDept)
+                .head(head)
+                .build();
+    }
 }
